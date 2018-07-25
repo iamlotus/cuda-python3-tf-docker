3700d78b6f8171356bd60cbeb6ec32bc7641cc39
iamlotus <iamlotus@gmail.com>
2018-07-25 19:34:14 +0800

    1

--- build.sh
+++ build.sh
@@ -1,4 +1,3 @@
 #!/bin/sh
 echo Build jinlo/tf:build
-# sudo nvidia-docker build --no-cache -t jinlo/tf:build  . -f Dockerfile.build
 sudo nvidia-docker build -t jinlo/tf:build  . -f Dockerfile.build
